860209ed44ef029004909997a80f7bd1570fbe09
Ahmed Mze <houdjiva@gmail.com>
2022-07-05 14:42:19 +0200

    Refacroring de main.go

--- main.go
+++ main.go
@@ -1,26 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"training.go/gencert/cert"
 	"training.go/gencert/html"
+	"training.go/gencert/pdf"
 )
 
 func main() {
+	outputType := flag.String("type", "pdf", "Output type of the certificate.")
+	flag.Parse()
+
+	var saver cert.Saver
+	var err error
+	switch *outputType {
+	case "html":
+		saver, err = html.New("output")
+	case "pdf":
+		saver, err = pdf.New("output")
+	default:
+		fmt.Printf("Unknown output type. got=%v \n", *outputType)
+	}
+	if err != nil {
+		fmt.Printf("Could not create generator: %v", err)
+		os.Exit(1)
+	}
+
 	c, err := cert.New("Golang programming", "Bob Dylan", "2018-06-21")
 	if err != nil {
 		fmt.Printf("Error during certificate creation: %v", err)
 		os.Exit(1)
 	}
-
-	var saver cert.Saver
-	//saver, err = pdf.New("output")
-	saver, err = html.New("output")
+	err = saver.Save(*c)
 	if err != nil {
-		fmt.Printf("Error during pdf generator creation: %v", err)
-		os.Exit(1)
+		return
 	}
-	saver.Save(*c)
 }
